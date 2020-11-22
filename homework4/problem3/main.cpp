5da1b70b70833a943fe22752ac448adae9cc15b4
AvocadoOnToast <czpereyra@ucdavis.edu>
2020-11-22 03:05:32 -0800

    1D fourier transform

--- homework4/problem3/main.cpp
+++ homework4/problem3/main.cpp
@@ -1,6 +1,6 @@
 #include <cstdlib>    // c++
 #include <cstdio>     // c++
-#include <omp.h>
+//#include <omp.h>
 #include <vector>     // c++
 #include <algorithm>  // c++
 #include <iostream>   // c++
@@ -15,6 +15,7 @@
 //      ./test
 //
 
+#define N 60
 #define NX 60
 #define NY 60
 #define SCALE 10
@@ -25,23 +26,24 @@ void write2file(int i, int newlineflag, double x, double y, double z);
 
 int main(int argc, char** argv) {
     double x, y;
-    double xmin = -SCALE*2.4; double xmax = SCALE*2.4; double ymin = -SCALE*2.4; double ymax = SCALE*2.4;
-    double dx = (xmax - xmin) / NX; double dy = (ymax - ymin) / NY;
+    double xmin = -SCALE*2.4; double xmax = SCALE*2.4;
+    double ymin = -SCALE*2.4; double ymax = SCALE*2.4;
+    double dx = (xmax - xmin) / NX; 
+    double dy = (ymax - ymin) / NY;
     double rx[2] = {2.4, 1.2};
     double ry[2] = {0, 2.078};
-    double theta_x = 2*M_PI / NX;
-    double theta_y = 2*M_PI / NY;
-    double greal, gimag;
+    double theta   = 2*M_PI / N;
+    double freal, fimag;
     double hreal, himag;
     double zreal, zimag, zabs;
 
-    int n = 1;
     double **address = (double**) malloc(sizeof(double*)*2);
-    double *xj = (double*) malloc(sizeof(double)*n);
-    double *yj = (double*) malloc(sizeof(double)*n);
+    double *xj = (double*) malloc(sizeof(double)*1);
+    double *yj = (double*) malloc(sizeof(double)*1);
     
     // input signal
-    address = inputsignal("noisyspectrum.dat", address, xj, yj, &n);
+    int n;
+    inputsignal("noisyspectrum.dat", address, xj, yj, &n);
     xj = address[0];
     yj = address[1];
 
@@ -53,47 +55,28 @@ int main(int argc, char** argv) {
     printf("M_PI = %lf\n", M_PI);
     printf("n = %d\n", n);
 
+    // fourier transform
+    double *ck = (double*) malloc(sizeof(double)*n);
+
+    // nk = nj = n
     for(int k=0; k<n; k++) {
-        //printf("xj[%d] = %lf yj[%d] = %lf\n", k, xj[k], k, yj[k]);
-    }
-    // N_l = N_j = Nx
-    // N_m = N_k = Ny
-    /*
-    int j, k;
-    for(j=0; j < NX; j++) {
-        for(k=0; k < NY; k++) {
-            // l, m loop
-            zreal = 0;
-            zimag = 0;
-            zabs = 0;
-            for(int l=0; l < NX; l++) {
-                for(int m=0; m < NY; m++) {
-                    x = xmin + l*dx;
-                    y = ymin + m*dy;
-                    greal = f_lm(x,y)*cos(theta_x*k*m);
-                    gimag = f_lm(x,y)*sin(theta_y*k*m);
-                    hreal = sin(theta_x*j*l);
-                    himag = cos(theta_y*j*m);
-
-                    zreal += greal*hreal;
-                    zimag += gimag*himag;
-                }
-            }
-            zabs = sqrt(pow(zreal, 2) + pow(zimag, 2));
-            //printf("j = %d k = %d zreal = %lf\n", j, k, zreal);
-            write2file(j+k, 0, j, k, zabs); // 2FT value
+        freal = 0;
+        fimag = 0;
+        for(int j=0; j<n; j++) {
+            freal += yj[j]*cos(k*theta*xj[j]);
+            // + yjimag[j]*sin(k*theta*j);
+            fimag += -yj[j]*sin(k*theta*xj[j]); 
+            // + yjimag[j]*sin(k*theta*xj[j]);
         }
-        //printf("\n");
-        write2file(j+k, 1, j, k, zabs); // next line in dat file
+        ck[k] = freal;
+        write2file(k, 0, k, ck[k], 0); // write [k] [0] [x] [y] [z]
     }
-    */
 
-    // FREE MEMORY
+    // free memory
     free(address[0]);
     free(address[1]);
     free(address);
-    //free(xj);
-    //free(yj);
+    free(ck);
 }
 
 // INPUT
@@ -106,7 +89,10 @@ double** inputsignal(string filename, double **addr, double *xj, double *yj, int
     int index = 0;
 
     // IO
-    if(!infile.is_open()) exit(1);
+    if(!infile.is_open()) {
+        printf("input file not found\n");
+        exit(1);
+    }
     //std::getline(infile, line); std::istringstream iss(line);
     //std::cout << "natoms = " << natoms << "\n";
     //std::getline(infile, line);  // line 2
@@ -116,7 +102,7 @@ double** inputsignal(string filename, double **addr, double *xj, double *yj, int
         // read x y z coords
         std::istringstream iss(line);
 
-        if (iss >> x >> y) // cout << "line = "<< index << " x = " << x << " y = " << y << "\n";
+        if (iss >> x >> y) //cout << "line = "<< index << " x = " << x << " y = " << y << "\n";
 
         xj[index] = double(x);
         yj[index] = double(y);
