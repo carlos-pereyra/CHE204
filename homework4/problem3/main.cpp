af6576382e5f63acb3ba90e14ae9c32dab59f072
AvocadoOnToast <pereyrazcarlos@gmail.com>
2020-11-23 20:41:24 -0800

    results for p3

--- homework4/problem3/main.cpp
+++ homework4/problem3/main.cpp
@@ -1,3 +1,6 @@
+/*
+ * Made by Carlos Pereyra Ph.D. student, UC Davis, CHE 204 2020
+ */
 #include <cstdlib>    // c++
 #include <cstdio>     // c++
 //#include <omp.h>
@@ -24,14 +27,16 @@ using namespace std;
 
 double** inputsignal(string filename, double **addr, double *xj, double *yj, int *n);
 double w(int j, int n);
-double ww(int j, int n);
+double gauss(int j, int n);
 void write2file(string filename, int i, int newlineflag, double x, double y, double z, double q);
 
 int main(int argc, char** argv) {
     double freal, fimag, fabs;
-    double inv_freal, inv_fimag, inv_fabs;
+    double inv_Freal, inv_Fimag, inv_Fabs;
+    double inv_Freal_w, inv_Fimag_w, inv_Fabs_w;
+    double inv_Freal_gauss, inv_Fimag_gauss, inv_Fabs_gauss;
 
-    double **address = (double**) malloc(sizeof(double*)*2);
+    double **address = (double**) malloc(sizeof(double*)*2); // points to xj and yj
     double *xj = (double*) malloc(sizeof(double)*1);
     double *yj = (double*) malloc(sizeof(double)*1);
     
@@ -57,23 +62,23 @@ int main(int argc, char** argv) {
     for(int k=0; k<n; k++) {
         freal = 0;
         fimag = 0;
-        inv_freal = 0;
-        inv_fimag = 0;
+        //inv_Freal = 0;
+        //inv_Fimag = 0;
         for(int j=0; j<n; j++) {
             // FT
             freal += yj[j]*cos(k*theta*j);  // + yjimag[j]*sin(k*theta*j); // (if there were an imaginary part)
             fimag += -yj[j]*sin(k*theta*j); // + yjimag[j]*sin(k*theta*xj[j]); // (if there were an imaginary part)
 
             // inv FT
-            inv_freal += (cos(xj[k]*theta*j)*(yj[k]*cos(xj[k]*theta*j)) + (yj[k]*sin(xj[k]*theta*j))*sin(xj[k]*theta*j))* w(j,n);
-            inv_fimag += (sin(xj[k]*theta*j)*(yj[k]*cos(xj[k]*theta*j)) - (yj[k]*sin(xj[k]*theta*j))*cos(xj[k]*theta*j))* w(j,n);
+            //inv_Freal += (cos(xj[k]*theta*j)*(yj[k]*cos(xj[k]*theta*j)) + (yj[k]*sin(xj[k]*theta*j))*sin(xj[k]*theta*j))* w(j,n);
+            //inv_Fimag += (sin(xj[k]*theta*j)*(yj[k]*cos(xj[k]*theta*j)) - (yj[k]*sin(xj[k]*theta*j))*cos(xj[k]*theta*j))* w(j,n);
         }
         // FT
         fabs = sqrt(pow(freal, 2) + pow(fimag, 2));
         ck_real[k] = freal / sqrt(2*M_PI);
         ck_imag[k] = fimag / sqrt(2*M_PI);
         // inverse FT
-        inv_fabs = sqrt(pow(inv_freal, 2) + pow(inv_fimag, 2));
+        //inv_Fabs = sqrt(pow(inv_Freal, 2) + pow(inv_Fimag, 2));
         // write [filename] [index] [newlineflag] [val1] [val2] [val3] [val4]
         write2file("data/signal.dat", k, 0, xj[k], yj[k], 0, 0);
         write2file("data/ft.dat", k, 0, k, ck_real[k], k, ck_imag[k]);
@@ -82,21 +87,48 @@ int main(int argc, char** argv) {
 
     // inverse fourier transform
     for(int k=0; k<n; k++) {
-        inv_freal = 0;
-        inv_fimag = 0;
+        inv_Freal       = 0;    // original - inverse fourier transform (real component)
+        inv_Fimag       = 0;    // original - inverse fourier transform (imaginary component)
+        inv_Freal_w     = 0;    // heaviside filtered - inverse fourier transform (real component)
+        inv_Fimag_w     = 0;    // heaviside filtered - inverse fourier transform (imaginary component)
+        inv_Freal_gauss = 0;    // gaussian filtered - inverse fourier transform (real component)
+        inv_Fimag_gauss = 0;    // gaussian filtered - inverse fourier transform (imaginary component)
         //for(int j=(-n/2); j<(n/2); j++) {
         for(int j=0; j<n; j++) {
-            //theta   = 2*M_PI / (double) n;
             double phase = k*theta*j;
-            inv_freal += (cos(phase)*ck_real[j] - sin(phase)*ck_imag[j]) * w(j,n); // weight function
-            inv_fimag += (sin(phase)*ck_real[j] + cos(phase)*ck_imag[j]) * w(j,n); // weight function
+            double Re = (cos(phase)*ck_real[j] - sin(phase)*ck_imag[j]);
+            double Im = (sin(phase)*ck_real[j] + cos(phase)*ck_imag[j]);
+            inv_Freal       += Re;
+            inv_Fimag       += Im; 
+            inv_Freal_w     += Re * w(j,n); 
+            inv_Fimag_w     += Im * w(j,n);
+            inv_Freal_gauss += Re * gauss(j,n); 
+            inv_Fimag_gauss += Im * gauss(j,n);
         }
-        inv_freal *= sqrt(2*M_PI) / (DUTY * n);
-        inv_fimag *= sqrt(2*M_PI) / (DUTY * n);
-        inv_fabs = sqrt(pow(inv_freal, 2) + pow(inv_fimag, 2));
+        // magnitude
+        inv_Fabs        = sqrt(pow(inv_Freal, 2) + pow(inv_Fimag, 2));
+        inv_Fabs_w      = sqrt(pow(inv_Freal_w, 2) + pow(inv_Fimag_w, 2));
+        inv_Fabs_gauss  = sqrt(pow(inv_Freal_gauss, 2) + pow(inv_Fimag_gauss, 2));
+        // normalize
+        inv_Fabs        *= sqrt(2*M_PI) / n;
+        inv_Freal       *= sqrt(2*M_PI) / n;
+        inv_Fimag       *= sqrt(2*M_PI) / n;
+
+        inv_Fabs_w      *= sqrt(2*M_PI) / (DUTY * n);
+        inv_Freal_w     *= sqrt(2*M_PI) / (DUTY * n);
+        inv_Fimag_w     *= sqrt(2*M_PI) / (DUTY * n);
+
+        inv_Fabs_gauss  *= sqrt(2*M_PI) / n;
+        inv_Freal_gauss *= sqrt(2*M_PI) / n;
+        inv_Fimag_gauss *= sqrt(2*M_PI) / n;
         // write [filename] [index] [newlineflag] [val1] [val2] [val3] [val4]
-        write2file("data/inv_ft.dat", k, 0, xj[k], inv_freal, xj[k], inv_fimag);
-        write2file("data/inv_ft_abs.dat", k, 0, xj[k], inv_fabs, 0, 0);
+        write2file("data/inv_ft_abs.dat", k, 0, xj[k], inv_Fabs, 0, 0);
+        write2file("data/inv_ft_w_abs.dat", k, 0, xj[k], inv_Fabs_w, 0, 0);
+        write2file("data/inv_ft_gauss_abs.dat", k, 0, xj[k], inv_Fabs_gauss, 0, 0);
+        //
+        write2file("data/inv_ft.dat", k, 0, xj[k], inv_Freal, xj[k], inv_Fimag);
+        write2file("data/inv_ft_w.dat", k, 0, xj[k], inv_Freal_w, xj[k], inv_Fimag_w);
+        write2file("data/inv_ft_gauss.dat", k, 0, xj[k], inv_Freal_gauss, xj[k], inv_Fimag_gauss);
     }
 
     //double complex pk[n];
@@ -166,7 +198,8 @@ double w(int j, int n) {
     return value;
 }
 
-double ww(int j, int n) {
+double gauss(int j, int n) {
+    double sigma = 0.1;
     return exp(-0.5*pow(2*n / (sigma * (n-1)), 2));
 }
 
